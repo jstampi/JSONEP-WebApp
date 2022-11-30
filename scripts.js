4cc006a069900e6b30194df3102c1eb45b0e91fc
jstampi <jovy.estampador@gmail.com>
2022-11-30 21:06:54 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -14,15 +14,15 @@ function testPost() {
     var url = "https://script.google.com/macros/s/AKfycbxertJ-M3g9l8QkhQG5JDRCz_QO_udmqvkpLCZu9X5p9wuVdrAua_K32Q9DpBDuhRnJ/exec";
     
     fetch(url, {
-        method: 'POST',
-        mode: 'no-cors',
-        cache: 'no-cache',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        redirect: 'follow',
-        body:JSON.stringify().        
-    });
+            method: 'POST',
+            mode: 'no-cors',
+            cache: 'no-cache',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            redirect: 'follow',
+            body:JSON.stringify()        
+          });
 }
           
           //{first:"Macron", phone:"88-555-1212", last:"Putin"}
